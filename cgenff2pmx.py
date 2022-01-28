e8116421fb3906346756009dc7ea899f31469869
Adrien H. Cerdan <cerdan.adrien@gmail.com>
2022-01-28 17:19:33 +0100

    Add files via upload

---
+++ cgenff2pmx.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys, getopt
+
+def read_bonds(bond,prm_file):
+    file2 = open(prm_file, 'r')
+    Lines2 = file2.readlines()
+    part_bonds=0
+    
+    for line in Lines2:
+        
+        if not line.strip(): #skip empty line
+            continue
+            
+        LL=line.strip()
+        
+        # find the bonds section
+        if LL == "[ bondtypes ]":
+            part_bonds=1
+            continue
+
+        if part_bonds==1:
+            if LL[0]==";":
+                continue
+            elif LL[0]=="[": # end of the bonds section
+                part_bonds=0
+                continue
+            else:
+                # split the string
+                bond_ff=LL.split()[0:]
+                # test the atoms involved in the bond in both direction
+                if (bond_ff[0:2] == bond) or (bond_ff[0:2] == bond[::-1]):
+                    # return the bond parameter from the .prm
+                    return bond_ff
+
+
+
+
+def read_angles(angle,prm_file):
+    # similar to read bond but with 3 atoms involved
+    file2 = open(prm_file, 'r')
+    Lines2 = file2.readlines()
+    part_angles=0
+    
+    for line in Lines2:
+
+        if not line.strip(): #skip empty line
+            continue
+            
+        LL=line.strip()
+
+        if LL == "[ angletypes ]":
+            part_angles=1
+            continue
+        if part_angles==1:
+            if LL[0]==";":
+                continue
+            elif LL[0]=="[":
+                part_angles=0
+                continue
+            else:
+                angle_ff=LL.split()[0:]
+                if (angle_ff[0:3] == angle) or (angle_ff[0:3] == angle[::-1]) :
+                    return angle_ff
+
+
+
+
+def read_dihedrals(dihedral,prm_file):
+    # similar to read bond but with 4 atoms involved
+    # also works with duplicated dihedral section with impropers from cgenff, but all dihedrals are merged
+    file2 = open(prm_file, 'r')
+    Lines2 = file2.readlines()
+    part_dihedrals=0
+    
+    for line in Lines2:
+
+        if not line.strip(): #skip empty line
+            continue
+            
+        LL=line.strip()
+
+
+        if LL == "[ dihedraltypes ]":
+            part_dihedrals=1
+            continue
+
+        if part_dihedrals==1:
+            if LL[0]==";":
+                continue
+            elif LL[0]=="[":
+                part_dihedrals=0
+                continue
+            else:
+                dihedral_ff=LL.split()[0:]
+                if (dihedral_ff[0:4] == dihedral) or (dihedral_ff[0:4] == dihedral[::-1]):
+                    return dihedral_ff
+
+
+
+
+# MAIN #
+# should handles errors if the bond-angle-dihedral types are missing in the .prm while using read_*
+
+
+def main(argv):
+    print(' $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+    print(' $            CGENFF2PMX.py          $')
+    print(' $ Parse .itp and .prm to merge them $')
+    print(' $ from cgenff_charmm2gmx.py to PMX  $')
+    print(' $ By Adrien Cerdan 2022             $')
+    print(' $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+
+    inputitp = ''
+    inputprm = ''
+    outputitp = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:p:o:",["iitpfile=","iprmfile=","oitpfile="])
+    except getopt.GetoptError:
+        print('cgenff2pmx.py -i <inputfileITP> -p <inputfilePRM> -o <outputfileITP>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('cgenff2pmx.py -i <inputfileITP> -p <inputfilePRM> -o <outputfileITP>')
+            sys.exit()
+        elif opt in ("-i", "--iitpfile"):
+            inputitp = arg
+        elif opt in ("-p", "--iprmfile"):
+            inputprm = arg
+        elif opt in ("-o", "--oitpfile"):
+            outputitp = arg
+    print('Input itp file is ', inputitp)
+    print('Input prm file is ', inputprm)
+    print('Output itp file is ', outputitp)
+
+    file1 = open(inputitp, 'r')
+    Lines1 = file1.readlines()
+
+    count = 0
+    part_moleculetype=0
+    part_atoms=0
+    part_bonds=0
+    part_pairs=0
+    part_angles=0
+    part_dihedrals=0
+    dict_atoms={}
+    with open(outputitp, 'w') as f:
+        f.write('; $ CGENFF2PMX.py $\n')
+        f.write('; Parse .itp and .prm to merge them\n')
+        f.write('; from cgenff_charmm2gmx.py to PMX\n')
+        f.write('; By Adrien Cerdan 2022 \n')
+        f.write('\n')
+    # Read lines
+    for line in Lines1:
+        if not line.strip(): #skip empty line
+            continue
+        LL=line.strip()  
+
+        # Enter moleculetype section: check if it can contains some usefull information but should be ok
+        if LL == "[ moleculetype ]":
+            if part_moleculetype==0:
+                #print('moleculetype')
+                with open(outputitp, 'a') as f:
+                    f.writelines(LL)
+                part_moleculetype=1
+                continue 
+        if part_moleculetype==1:
+            if LL[0]==";":
+                with open(outputitp, 'a') as f:
+                    f.writelines('\n')
+                    f.writelines(LL+'\n')
+                continue
+            elif LL=="[ atoms ]":
+                with open(outputitp, 'a') as f:
+                    f.writelines('\n')
+                    f.writelines(LL+'\n')
+                part_moleculetype=0
+                part_atoms=1
+                #print("atoms")
+                continue
+            else:
+                pairs_out=LL.split()
+                #print(pairs_out)
+                with open(outputitp, 'a') as f:
+                    for line in pairs_out:
+                        f.write('    '+"".join(line) + "\t")
+                    f.writelines('\n')
+                continue
+
+        # Enter atoms section: nothing to do, just copy
+        if part_atoms==1:
+            if LL[0]==";":
+                with open(outputitp, 'a') as f:
+                    f.writelines(LL+'\n')
+                continue
+            elif LL[0]=="[":
+                with open(outputitp, 'a') as f:
+                    f.writelines('\n')
+                    f.writelines(LL+'\n')
+                part_atoms=0
+                part_bonds=1
+                #print("bonds")
+                continue
+            else:
+                #print(LL)
+                with open(outputitp, 'a') as f:
+                    f.writelines('    '+LL+'\n')
+                # Create the dictionary where atom number is associated to an atom type to read .prm
+                idx_atom=int(LL.split()[0])
+                atom_type=LL.split()[1]
+                dict_atoms[idx_atom]=atom_type
+
+        # Enter bonds section: use read_bonds on .prm then copy
+        if part_bonds==1:
+            if LL[0]==";":
+                with open(outputitp, 'a') as f:
+                    f.writelines(LL+'\n')
+                continue
+            elif LL=="[ pairs ]":
+                with open(outputitp, 'a') as f:
+                    f.writelines('\n')
+                    f.writelines(LL+'\n')
+                part_bonds=0
+                part_pairs=1
+                #print("pairs")
+                continue
+            else:
+                # read atom indexes forming the bond
+                bond_idx=LL.split()[0:2]
+                bond=[dict_atoms[int(bond_idx[0])],dict_atoms[int(bond_idx[1])]]
+                # read the parameters from .prm using read_bonds
+                bond_ff=read_bonds(bond,inputprm)
+                bond_ff[0]=bond_idx[0]
+                bond_ff[1]=bond_idx[1]
+                with open(outputitp, 'a') as f:
+                    for line in bond_ff:
+                        f.write('    '+"".join(line) + "\t")
+                    f.writelines('\n')
+                #print(bond_ff)
+                continue
+
+        # Enter pairs section: nothing tot do I guess, just copy
+        if part_pairs==1:
+            if LL[0]==";":
+                with open(outputitp, 'a') as f:
+                    f.writelines(LL+'\n')
+                continue
+            elif LL=="[ angles ]":
+                with open(outputitp, 'a') as f:
+                    f.writelines('\n')
+                    f.writelines(LL+'\n')
+                part_pairs=0
+                part_angles=1
+                #print("angles")
+                continue
+            else:
+                pairs_out=LL.split()
+                #print(pairs_out)
+                with open(outputitp, 'a') as f:
+                    for line in pairs_out:
+                        f.write('    '+"".join(line) + "\t")
+                    f.writelines('\n')
+                continue
+
+        # Enter Angles section: use read_angles to extract them from .prm
+        if part_angles==1:
+            if LL[0]==";":
+                with open(outputitp, 'a') as f:
+                    #f.writelines('\n')
+                    f.writelines(LL+'\n')
+                continue
+            elif LL=="[ dihedrals ]":
+                with open(outputitp, 'a') as f:
+                    f.writelines('\n')
+                    f.writelines(LL+'\n')
+                part_angles=0
+                part_dihedrals=1
+                #print("dihedrals")
+                continue
+            else:
+                # like in bonds but with angles
+                angle_idx=LL.split()[0:3]
+                angle=[dict_atoms[int(angle_idx[0])],dict_atoms[int(angle_idx[1])],dict_atoms[int(angle_idx[2])]]
+                angle_ff=read_angles(angle,inputprm)
+                angle_ff[0]=angle_idx[0]
+                angle_ff[1]=angle_idx[1]
+                angle_ff[2]=angle_idx[2]
+                with open(outputitp, 'a') as f:
+                    for line in angle_ff:
+                        f.write('    '+"".join(line) + "\t")
+                    f.writelines('\n')
+                #print(angle_ff)
+                continue
+
+        # enter in dihedral section: read_dihedrals is called to extract them from .prm
+        if part_dihedrals==1:
+            if LL[0]==";":
+                with open(outputitp, 'a') as f:
+                    #f.writelines('\n')
+                    f.writelines(LL+'\n')
+                continue
+            elif LL=="[ dihedrals ]":
+                with open(outputitp, 'a') as f:
+                    f.writelines('\n')
+                    f.writelines(LL+'\n')
+                part_angles=0
+                part_dihedrals=1
+                #print("dihedrals")
+                continue
+            else:
+                # like bonds and angles
+                dihedral_idx=LL.split()[0:4]
+                dihedral=[dict_atoms[int(dihedral_idx[0])],dict_atoms[int(dihedral_idx[1])],dict_atoms[int(dihedral_idx[2])],dict_atoms[int(dihedral_idx[3])]]
+                dihedral_ff=read_dihedrals(dihedral,inputprm)
+                dihedral_ff[0]=dihedral_idx[0]
+                dihedral_ff[1]=dihedral_idx[1]
+                dihedral_ff[2]=dihedral_idx[2]
+                dihedral_ff[3]=dihedral_idx[3]
+                with open(outputitp, 'a') as f:
+                    for line in dihedral_ff:
+                        f.write('    '+"".join(line) + "\t")
+                    f.writelines('\n')
+                #print(dihedral_ff)
+                continue
+
+
+#print(dict_atoms)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+
